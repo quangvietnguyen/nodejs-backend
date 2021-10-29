674e197e7f91fdaba3c1f1312df97060c6f64c47
nguye374 <nguye374@sheridancollege.ca>
2021-10-29 03:10:11 -0400

    fix the unique validator

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+//const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,6 +9,6 @@ const userSchema = new mongoose.Schema({
   places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place'}]
 });
 
-userSchema.plugin(uniqueValidator);
+//userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
