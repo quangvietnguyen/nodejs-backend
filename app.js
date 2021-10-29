df37a531b92811298f5d09285d8c1882408a321f
nguye374 <nguye374@sheridancollege.ca>
2021-10-28 21:27:00 -0400

    Oct 28

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require('./routes/users-routes');
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // => /api/places/...
-app.use('/api/users', usersRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
@@ -25,4 +26,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "Something went wrong!" });
 });
 
-app.listen(4000);
+mongoose
+  .connect('mongodb+srv://vietnguyen:Nexaple92@cluster0.x1sv0.mongodb.net/database?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((e) => {
+    console.log(e)
+  });
