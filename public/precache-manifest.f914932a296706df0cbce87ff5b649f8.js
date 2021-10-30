22bc61c66f58b5a0b9492741bbe846f67fd6e2c3
nguye374 <nguye374@sheridancollege.ca>
2021-10-30 01:14:43 -0400

    add react to deploy same server

---
+++ public/precache-manifest.f914932a296706df0cbce87ff5b649f8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44e9a72f23a79d6d522f5f295fb0430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e8bbd36c429692206a",
+    "url": "/static/css/0.18c18cb9.chunk.css"
+  },
+  {
+    "revision": "09b5caa922c2d6dfb9ec",
+    "url": "/static/css/3.a8c58b36.chunk.css"
+  },
+  {
+    "revision": "45af145073e8667c866d",
+    "url": "/static/css/5.3bcb4508.chunk.css"
+  },
+  {
+    "revision": "021dd102ad3d017024c7",
+    "url": "/static/css/6.49c00d8f.chunk.css"
+  },
+  {
+    "revision": "db12a52366ef2c778747",
+    "url": "/static/css/7.44e30e40.chunk.css"
+  },
+  {
+    "revision": "83cdbe05d3c54c085839",
+    "url": "/static/css/8.7b688411.chunk.css"
+  },
+  {
+    "revision": "823cfae530ddba3b7f4e",
+    "url": "/static/css/9.e1fa2d0d.chunk.css"
+  },
+  {
+    "revision": "551a24a442fdb6a03a18",
+    "url": "/static/css/main.f7db2440.chunk.css"
+  },
+  {
+    "revision": "b1e8bbd36c429692206a",
+    "url": "/static/js/0.0d498038.chunk.js"
+  },
+  {
+    "revision": "09b5caa922c2d6dfb9ec",
+    "url": "/static/js/3.e0a77a2d.chunk.js"
+  },
+  {
+    "revision": "464661672774cab4a16a",
+    "url": "/static/js/4.0758a174.chunk.js"
+  },
+  {
+    "revision": "45af145073e8667c866d",
+    "url": "/static/js/5.19054820.chunk.js"
+  },
+  {
+    "revision": "021dd102ad3d017024c7",
+    "url": "/static/js/6.98ba44d8.chunk.js"
+  },
+  {
+    "revision": "db12a52366ef2c778747",
+    "url": "/static/js/7.d8427fc4.chunk.js"
+  },
+  {
+    "revision": "83cdbe05d3c54c085839",
+    "url": "/static/js/8.71017292.chunk.js"
+  },
+  {
+    "revision": "823cfae530ddba3b7f4e",
+    "url": "/static/js/9.0b49e1ea.chunk.js"
+  },
+  {
+    "revision": "551a24a442fdb6a03a18",
+    "url": "/static/js/main.85072851.chunk.js"
+  },
+  {
+    "revision": "f7c2ef794a5e0c3002f7",
+    "url": "/static/js/runtime-main.a6b5f8c9.js"
+  }
+]);
