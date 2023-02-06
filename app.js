21e95f217d113e53b4809f5235a462de349ace06
nguye374 <nguye374@sheridancollege.ca>
2023-02-06 00:18:39 -0500

    retrieved cors handler

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/http-error");
-const mongoose = require("mongoose");
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
@@ -14,22 +14,22 @@ app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static(path.join('public')));
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  next();
+});
 
-app.use("/api/places", placesRoutes); // => /api/places/...
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes); // => /api/places/...
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname, 'public','index.html'))
-})
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 // app.use((req, res, next) => {
 //   const error = new HttpError("Could not find this route.", 404);
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
   }
 
   res.status(error.code || 500);
-  res.json({ message: error.message || "Something went wrong!" });
+  res.json({ message: error.message || 'Something went wrong!' });
 });
 
 mongoose
